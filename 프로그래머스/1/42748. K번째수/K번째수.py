62397cb26740bc3ef26921e619631fa0bd45be18
HYUN <130816706+vvzvvv@users.noreply.github.com>
2025-04-02 23:32:51 +0900

    Update K번째수.py

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,7 +2,7 @@ def solution(array, commands):
     answer = []
     
     for c in commands:
-        # i = [2,5,3],[4,4,1],[1,7,3]
+        # c = [2,5,3],[4,4,1],[1,7,3]
         i, j, k = c[0], c[1], c[2]
         arr = array[i-1:j]
         arr.sort()
