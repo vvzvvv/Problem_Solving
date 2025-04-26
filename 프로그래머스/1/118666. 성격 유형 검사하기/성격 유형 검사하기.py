64a17119e037602fecfc92190aea440eac899dd4
HYUN <130816706+vvzvvv@users.noreply.github.com>
2025-04-26 15:41:13 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.31 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,18 +1,3 @@
 def solution(survey, choices):
-    지표 = [('R', 'T'), ('C', 'F'), ('J', 'M'), ('A', 'N')]
-    d = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
-    
-    n = len(survey)
-    for i in range(n):
-        if choices[i] >= 5:
-            d[survey[i][1]] += choices[i] - 4
-        elif choices[i] <= 3:
-            d[survey[i][0]] += abs(choices[i] - 4)
-    
-    result = []
-    for i, j in 지표:
-        if d[i] >= d[j] : result.append(i)
-        else: result.append(j)
-    answer = ''.join(result)
-    
+    answer = ''
     return answer
