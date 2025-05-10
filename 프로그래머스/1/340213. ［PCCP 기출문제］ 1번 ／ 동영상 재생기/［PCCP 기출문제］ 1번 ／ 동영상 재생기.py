81a2c4d8494510f0eeaf891e5e5c792778256d95
HYUN <130816706+vvzvvv@users.noreply.github.com>
2025-05-10 19:24:51 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.05 ms, Memory: 9.49 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -0,0 +1,36 @@
+def to_sec(time):
+    m, s = time.split(':')
+    return int(m) * 60 + int(s)
+
+def to_str(time):
+    m = time // 60
+    s = time % 60
+    if m < 10: m = '0' + str(m)
+    if s < 10: s = '0' + str(s)
+    return str(m) + ':' + str(s)
+
+
+def solution(video_len, pos, op_start, op_end, commands):
+    answer = ''
+    video_len = to_sec(video_len)
+    op_start = to_sec(op_start)
+    op_end = to_sec(op_end)
+    pos = to_sec(pos)
+
+    for com in commands:
+        if op_start <= pos <= op_end:
+            pos = op_end
+        if com == "prev":
+            pos -= 10
+            if pos < 0:
+                pos = 0
+            elif op_start <= pos <= op_end:
+                pos = op_end
+        elif com == "next":
+            pos += 10
+            if pos > video_len:
+                pos = video_len
+            elif op_start <= pos <= op_end:
+                pos = op_end
+            
+    return to_str(pos)
