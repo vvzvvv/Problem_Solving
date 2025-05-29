bedd48276b1d2e80ae3a967a36a65603a74e3abb
HYUN <130816706+vvzvvv@users.noreply.github.com>
2025-05-30 01:56:51 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,4 +1,7 @@
 function solution(num_list) {
-    var answer = num_list.reverse();
+    var answer = [];
+    for (let i = num_list.length - 1; i >= 0; i--) {
+        answer.push(num_list[i]);
+    }
     return answer;
 }
