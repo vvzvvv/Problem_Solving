10d8d38c2f5285ed57edde33b5831dbf00f78ae1
Seunghyun Kim <130816706+vvzvvv@users.noreply.github.com>
2026-01-19 23:59:05 +0900

    [Silver III] Title: 두 수의 합, Time: 84 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,15 +1,23 @@
 n = int(input())
 lst = list(map(int, input().split()))
-arr = [0 for _ in range(1000001)]
-for num in lst:
-    arr[num] = 1
-
+lst.sort()
+left = 0
+right = n-1
 x = int(input())
 
 result = 0
-for i in range(1000001):
-    if arr[i] and (x - i <= 1000000) and arr[x - i] and (x - i != i):
-        arr[i] = 0
+while 1:
+    if left >= right: break
+    
+    sum_num = lst[left] + lst[right]
+    
+    if sum_num == x:
         result += 1
+        left += 1
+        right -= 1
+    elif sum_num > x:
+        right -= 1
+    else:
+        left += 1
 
 print(result)
