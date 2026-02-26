c3ab064bee3fb6293cc7aabae0f9c40b80d30a26
Seunghyun Kim <130816706+vvzvvv@users.noreply.github.com>
2026-02-26 15:39:15 +0900

    [Silver III] Title: N과 M (3), Time: 1868 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,11 +1,14 @@
 n, m = map(int, input().split())
-arr = []
-def func(k):
-    if k == m:
-        print(*arr)
+arr = [num for num in range(1, n + 1)]
+path = [0 for _ in range(m)]
+
+def func(cnt):
+    if cnt == m:
+        print(*path)
         return
-    for i in range(1, n+1):
-        arr.append(i)
-        func(k+1)
-        arr.pop()
+    
+    for i in range(n):
+        path[cnt] = arr[i]
+        func(cnt + 1)
+
 func(0)
