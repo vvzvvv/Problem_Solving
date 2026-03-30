089554012db12b06d014d54f40594de9b73856d3
Seunghyun Kim <130816706+vvzvvv@users.noreply.github.com>
2026-03-30 23:23:54 +0900

    [level 2] Title: 타겟 넘버, Time: 602.38 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,18 @@
-from itertools import product
+from collections import deque
+
 def solution(numbers, target):
-    answer = 0
-    data = []
-    for num in numbers:
-        data.append((num, -num))
-    
-    s = set(product(*data))
-    for t in s:
-        if sum(t) == target:
-            answer += 1
-            
-    return answer
+    queue = deque()
+    queue.append((0, 0))  # (현재 인덱스, 현재까지의 합)
+    count = 0
+  
+    while queue:
+        idx, total = queue.popleft()
+
+        if idx == len(numbers):
+            if total == target:
+                count += 1
+        else:
+            queue.append((idx + 1, total + numbers[idx]))
+            queue.append((idx + 1, total - numbers[idx]))
+
+    return count
