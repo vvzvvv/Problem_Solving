2a419923f0b47618312c175256b0175f1b3a279c
Seunghyun Kim <130816706+vvzvvv@users.noreply.github.com>
2026-04-08 15:06:29 +0900

    [level 2] Title: 요격 시스템, Time: 406.13 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,11 +1,9 @@
 def solution(targets):
-    result = 0
+    answer = 0
     targets.sort(key=lambda x: x[1])
-    prev_end = 0
-
-    for t in targets:
-        if t[0] >= prev_end:
-            result += 1
-            prev_end = t[1]
-        
-    return result
+    e = 0
+    for start, end in targets:
+        if start >= e:
+            answer += 1
+            e = end
+    return answer
