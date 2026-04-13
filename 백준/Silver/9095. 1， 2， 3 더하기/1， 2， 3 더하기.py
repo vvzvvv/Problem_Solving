195b73b57ee59de102d5bd931e57895fe247d9e3
Seunghyun Kim <130816706+vvzvvv@users.noreply.github.com>
2026-04-13 23:00:03 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,8 +1,11 @@
 t = int(input())
-dp = [0] * 12
-dp[1], dp[2], dp[3] = 1, 2, 4
-for i in range(4, 12):
-    dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+dp = [0] * 11
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4, 11):
+    dp[i] = dp[i-3] + dp[i-2] + dp[i-1] 
 
 for _ in range(t):
     n = int(input())
