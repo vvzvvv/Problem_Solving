7fb91dc0afc2087f61c4d5ca8f9c026e84971156
Seunghyun Kim <130816706+vvzvvv@users.noreply.github.com>
2026-04-24 15:01:08 +0900

    [level 2] Title: 무인도 여행, Time: 13.05 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,34 +1,22 @@
+import sys 
+sys.setrecursionlimit(10000)
+
 def solution(maps):
     answer = []
-    row, col = len(maps), len(maps[0])
+    n, m = len(maps), len(maps[0])
+    visited = [[0] * m for _ in range(n)]
     
-    visited = [[False] * col for _ in range(row)]
-
     def dfs(x, y):
-        stack = [(x, y)]
-        food = 0
+        if not (0 <= x < n and 0 <= y < m) or maps[x][y] == 'X' or visited[x][y]:
+            return 0
         
-        while stack:
-            cx, cy = stack.pop()
-            
-            if visited[cx][cy]:
-                continue
-            
-            visited[cx][cy] = True
-            food += int(maps[cx][cy])
-            
-            for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-                nx, ny = cx+dx, cy+dy
-                if 0 <= nx < row and 0 <= ny < col and not visited[nx][ny] and maps[nx][ny] != "X":
-                    stack.append((nx, ny))
-        return food
-            
-    for i in range(row):
-        for j in range(col):
-            if maps[i][j] != "X" and visited[i][j] == False:
-                res = dfs(i, j)
-                if res: answer.append(res)
-    answer.sort()
+        visited[x][y] = 1
+        return int(maps[x][y]) + dfs(x-1, y) + dfs(x+1, y) + dfs(x, y-1) + dfs(x, y+1)
+    
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] != 'X' and not visited[i][j]:
+                answer.append(dfs(i, j))
     
     if not answer: return [-1]
-    return answer
+    return sorted(answer)
