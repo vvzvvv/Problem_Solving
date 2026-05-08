844f1caa2e34842595fe6af12690d65b47a849a3
Seunghyun Kim <130816706+vvzvvv@users.noreply.github.com>
2026-05-08 15:05:01 +0900

    [level 2] Title: 할인 행사, Time: 98.77 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -6,19 +6,8 @@ def solution(want, number, discount):
     for i in range(len(want)):
         d[want[i]] = number[i]
     
-    day10 = Counter(discount[:10])
-    if day10 == d: answer += 1
-    
-    for i in range(10, len(discount)):
-        if day10[discount[i-10]] == 1:
-            del day10[discount[i-10]]
-        else:
-            day10[discount[i-10]] -= 1
-                
-        if discount[i] in day10:
-            day10[discount[i]] += 1
-        else:
-            day10[discount[i]] = 1
-        
-        if day10 == d: answer += 1
+    for i in range(len(discount) - 9):
+        if d == Counter(discount[i:i+10]):
+            answer += 1
+            
     return answer
